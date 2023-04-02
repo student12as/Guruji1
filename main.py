75573c457a29015b1e2d6cc07834f05fe3fa1f27
kd2929 <110597375+kd2929@users.noreply.github.com>
2023-04-03 00:21:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -223,7 +223,7 @@ async def account_login(bot: Client, m: Message):
     x = await input.download()
     await input.delete(True)
 
-    path = f"./downloads/{m.chat.id}"
+    path = f"./downloads/{.chat.id}"
 
     try:    
         with open(x, "r") as f:
