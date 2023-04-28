347726c5bb167d4a90bbb3da046e4e6e2dd13b68
Vinisha <110597375+kd2929@users.noreply.github.com>
2023-04-28 15:29:47 +0530

    Update and rename main (2) (27).py to main.py

--- main.py
+++ main.py
@@ -39,14 +39,6 @@ import io
 
 
 bot = Client("bot",
-             bot_token=os.environ.get("BOT_TOKEN"),
-             api_id=int(os.environ.get("API_ID")),
-             api_hash=os.environ.get("API_HASH"))
-auth_users = [
-    int(chat) for chat in os.environ.get("AUTH_USERS").split(",") if chat != '']
-sudo_users = auth_users
-sudo_groups = [
-    int(chat) for chat in os.environ.get("GROUPS").split(",") if chat != '']
 
 keyboard = InlineKeyboardMarkup(
     [
